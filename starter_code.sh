b9569bb1cb967b64861763d6694d29a84a4375e9
Colm Flanagan <z5122189@unsw.edu.au>
2021-10-13 22:46:24 -0700

    added starter code script

--- starter_code.sh
+++ starter_code.sh
@@ -1,3 +1,3 @@
-source ~/starter_code/install/setup.bash &&
+source ./starter_code/install/setup.bash &&
 ros2 run comp3431_starter wallFollower &
 ros2 topic pub /cmd std_msgs/msg/String "{data : start}"
