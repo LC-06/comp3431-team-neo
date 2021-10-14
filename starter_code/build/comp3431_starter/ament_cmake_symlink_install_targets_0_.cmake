a3f8a7e10fcdf90f861c000832ed408932059454
Carlo P <33255812+carlo-p-tech@users.noreply.github.com>
2021-10-15 01:57:27 +1100

    Delete starter_code/build directory

--- starter_code/build/comp3431_starter/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/rsa2021/comp3431-team-neo/starter_code/build/comp3431_starter/wallFollower" "TARGETS" "wallFollower" "DESTINATION" "lib/comp3431_starter")
