77f3a93bb54081e69aba714d328ec2801a8d7e63
Leo Cao <z5261846@ad.unsw.edu.au>
2021-11-08 01:49:27 +1100

    removed install, log, build

--- starter_code/build/point_pubsub/CMakeFiles/point_pubsub.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/point_pubsub"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/point_pubsub.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- starter_code/build/point_pubsub/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(point_pubsub_IDL_FILES "msg/Symbol.idl")
-set(point_pubsub_INTERFACE_FILES "msg/Symbol.msg")
