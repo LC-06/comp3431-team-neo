706eb6d7ae6b1d9bb773ffa3cd0a8dd1caadad3a
Leo Cao <z5261846@ad.unsw.edu.au>
2021-11-08 11:02:44 +1100

    new map

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # comp3431-team-neo
+
+- To install and build the project, run:
+
+    ```
+    cd starter_code
+    . install/setup.bash
+    colcon build --symlink-install
+    ```
--- turtlebot3_startup.sh
+++ turtlebot3_startup.sh
@@ -1,4 +1,5 @@
 source ~/turtlebot3_ws/install/setup.bash &&
-ros2 launch turtlebot3_gazebo turtlebot3_house.launch.py &
+ros2 launch comp3431_ass3_worlds house1.launch.py &
+# ros2 launch turtlebot3_gazebo turtlebot3_house.launch.py &
 ros2 launch turtlebot3_cartographer cartographer.launch.py use_sim_time:=true
 
