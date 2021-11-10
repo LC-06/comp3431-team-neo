b70fb57f8e48aad8b4584c0078c024ecb8496c1f
dean.hou98@hotmail.com <dean.hou98@hotmail.com>
2021-11-10 12:14:25 +1100

    calling topic to start once only

--- starter_code.sh
+++ starter_code.sh
@@ -2,4 +2,4 @@ source ./starter_code/install/setup.bash &&
 ros2 run comp3431_starter wallFollower &
 ros2 run zbar_ros barcode_reader &
 ros2 run point_pubsub point_tf &
-ros2 topic pub /cmd std_msgs/msg/String "{data : start}"
+ros2 topic pub /cmd std_msgs/msg/String "{data : start}" -1
