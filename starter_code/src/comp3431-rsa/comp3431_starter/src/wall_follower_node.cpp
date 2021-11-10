0489edc9dfd87979a887f7e4fb2cc19cc6eaa4e8
dean.hou98@hotmail.com <dean.hou98@hotmail.com>
2021-11-10 16:57:28 +1100

    stop command works and map saved

--- starter_code/src/comp3431-rsa/comp3431_starter/src/wall_follower_node.cpp
+++ starter_code/src/comp3431-rsa/comp3431_starter/src/wall_follower_node.cpp
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <wall_follower.h>
 
 int main(int argc, char** argv)
@@ -6,6 +7,10 @@ int main(int argc, char** argv)
 
 	rclcpp::init(argc, argv);
 	rclcpp::Node::SharedPtr nh = rclcpp::Node::make_shared("wall_follwer");
+	rclcpp::on_shutdown([] () {
+			system("ros2 run nav2_map_server map_saver_cli -f map_house1");
+		}
+	);
 
 	rclcpp::spin(std::make_shared<WallFollower>());
 	rclcpp::shutdown();
