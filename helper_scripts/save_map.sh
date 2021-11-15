d5efdfdd091f11ce20cafb92917fe184c4779470
srutid01 <62086118+srutid01@users.noreply.github.com>
2021-11-15 12:40:35 +1100

    Update save_map.sh

--- helper_scripts/save_map.sh
+++ helper_scripts/save_map.sh
@@ -1,2 +1,3 @@
 # ros2 topic pub /cmd std_msgs/msg/String "{data : stop}" -1
-ros2 run nav2_map_server map_saver_cli -f map_house1 --ros-args -p save_map_timeout:=10
+ros2 run nav2_map_server map_saver_cli -f map_house1 --ros-args -p save_map_timeout:=100
+ros2 run service_node service_node
