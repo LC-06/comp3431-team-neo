7ef1a9a3efeb8f1c24d7dc714a1b1811655b3ed6
srutid01 <62086118+srutid01@users.noreply.github.com>
2021-11-15 12:39:42 +1100

    Update publisher_member_function.cpp

--- starter_code/src/comp3431-rsa/point_pubsub/src/publisher_member_function.cpp
+++ starter_code/src/comp3431-rsa/point_pubsub/src/publisher_member_function.cpp
@@ -36,7 +36,6 @@ public:
     //   [this](geometry_msgs::msg::PointStamped::SharedPtr ball) {
     //     publisher_->publish(convert(ball));
     //   });
-
     subscriber_ = this->create_subscription<point_msg_interface::msg::Pointmsg>("/testPoint", 1, std::bind(&PointTf::convert, this, std::placeholders::_1));
     
     commandSub_ = this->create_subscription<std_msgs::msg::String>("cmd", 1, std::bind(&PointTf::callbackControl, this, std::placeholders::_1));
@@ -142,20 +141,42 @@ private:
   void callbackControl(const std_msgs::msg::String::SharedPtr command) {
     //RCLCPP_INFO(this->get_logger(), "Recieved %s message.\n", command->data.c_str());
     std::string message = std::string{command->data};
-    if(message == "stop") {
+    std::cout << "\nhello\nhello\n";
+    if(message == "pause") {
+      // std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("set_map_info_client");
       RCLCPP_INFO(this->get_logger(), "Point_pubsub(client) sending to server marker data");
       int send_marker_counter = 0;
       for(const auto& n : marker_map_) {
-        std::cout << send_marker_counter << "\n";
+        // std::cout << send_marker_counter << "\n";
         comp3431_interfaces::msg::QRCodeBlock oneMarker;
         oneMarker.text = n.first; // data
         oneMarker.pose.position.x = marker_map_[n.first][0].pose.position.x;
         oneMarker.pose.position.y = marker_map_[n.first][0].pose.position.y;
         oneMarker.pose.position.z = marker_map_[n.first][0].pose.position.z;
-        std::cout << oneMarker.text << " " << oneMarker.pose.position.z << std::endl;
-        // send_markers_.blocks[send_marker_counter] = oneMarker;
+        // std::cout << oneMarker.text << " " << oneMarker.pose.position.z << std::endl;
+        send_markers_.blocks[send_marker_counter] = oneMarker;
         send_marker_counter++;
         // std::cout << send_markers_.blocks[send_marker_counter-1].text << std::endl;
+      }
+
+      auto request = std::make_shared<comp3431_interfaces::srv::MapInfo::Request>();
+      request->blocks = send_markers_.blocks;
+       while (!client->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+          RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+          return;
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+      }
+
+      auto result = client->async_send_request(request);
+      // Wait for the result.
+      if (rclcpp::spin_until_future_complete(node, result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+      {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->res);
+      } else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
       }
 
       //while(wait for action)
@@ -177,6 +198,11 @@ private:
   visualization_msgs::msg::MarkerArray ma_barcodes;
   std::unordered_map<std::string, std::vector<visualization_msgs::msg::Marker>> marker_map_;
   comp3431_interfaces::srv::MapInfo::Request send_markers_;  
+
+  //Client node
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("set_map_info_client");
+  rclcpp::Client<comp3431_interfaces::srv::MapInfo>::SharedPtr client =
+    node->create_client<comp3431_interfaces::srv::MapInfo>("set_map_info");
 };
 
 int main(int argc, char * argv[])
