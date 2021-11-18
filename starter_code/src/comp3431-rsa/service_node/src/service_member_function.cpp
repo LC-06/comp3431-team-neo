dd3c4a1572cb33174736a730031e8b77427dd346
Colm Flanagan <z5122189@unsw.edu.au>
2021-11-17 16:42:05 -0800

    problem pddl generation

--- starter_code/src/comp3431-rsa/service_node/src/service_member_function.cpp
+++ starter_code/src/comp3431-rsa/service_node/src/service_member_function.cpp
@@ -2,19 +2,118 @@
 #include "comp3431_interfaces/srv/map_info.hpp"
 #include <iostream>
 #include <memory>
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
 
 void startPlanner(const std::shared_ptr<comp3431_interfaces::srv::MapInfo::Request> request,
           std::shared_ptr<comp3431_interfaces::srv::MapInfo::Response>      response)
 {
     std::cout << "\n\n\n\n\n\n" << std::endl;
-//   response->sum = request->a + request->b;
-response->res = 1;
+    response->res = 1;
+    // should probably change to reside within FF-X once we know it works and receive goal from action
+    std::string problemPath = "problem.pddl";
+    std::ofstream file;
+    file.open(problemPath);
+    file << "(define (problem moveitemtoroom)" << std::endl;
+    file << "  (:domain turtlebot3-domain)" << std::endl;
+    file << "  (:objects" << std::endl;
+    file << "    turtlebot - robot" << std::endl;
+    // is initial room, the room outside house? or just sample in sample-problem.pddl
+    // file << " initial-room - room" << std::endl;
+
+    auto objects = std::vector<std::string>{};
+    auto rooms = std::vector<std::string>{};
+    // Key is object, value is room - assuming items are unique
+    auto init = std::map<std::string, std::string>{};
+
     for(const auto& marker : request->blocks) {
+        auto text = marker.text;
+        // don't think we need pos to create problem.pddl
+        // auto pos_x = marker.pose.position.x;
+        // auto pos_y = marker.pose.position.y;
+        // auto pos_z = marker.pose.position.z;
+
+        auto words = std::vector<std::string>{};
+        // Split string into vector of words
+        std::string temp = "";
+        for(int i=0;i< static_cast<int>(text.length());++i){
+          
+          if(text[i]==' '){
+            words.push_back(temp);
+            temp = "";
+          }
+          else{
+            temp.push_back(text[i]);
+          }
+          
+        }
+        words.push_back(temp);
+
+        // Loop through words and push into objects or room vector
+        int index = 0;
+        std::string room_name;
+        for (auto itr = words.begin(); itr != words.end(); ++itr) {
+          // if first element in array, word is a room else items
+          if (index == 0) {
+            rooms.push_back(*itr);
+            room_name = *itr;
+          } 
+          else {
+            objects.push_back(*itr);
+            init.insert({ *itr, room_name });
+          } 
+
+          index++;
+        }
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\n %s", marker.text );
     }
-//   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-//                 request->a, request->b);
-//   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
+
+    // Add rooms to objects section
+    for (auto itr = rooms.begin(); itr != rooms.end(); ++itr) {
+      std::string room_line = "    " + *itr + " - room";
+      file << room_line << std::endl;
+    }
+    
+    // Add items to objects section - probs should use iterators but cbb to find documentation lol
+    std::string objects_string;
+    for (auto itr = objects.begin(); itr != objects.end(); ++itr) {
+      objects_string = objects_string + *itr + " ";
+    }
+
+    if (!objects_string.empty()) {
+      std::string objects_line = "    " + objects_string + " - item";
+      file << objects_line << std::endl;
+    }
+    
+    file << "  )\n" << std::endl;
+
+    // Init section
+    file << "  (:init" << std::endl;
+
+    // Should change - need to get location of turtlebot; probably use pose of blocks and pose of robot and convert
+    file << "    (at turtlebot initial-room)" << std::endl;
+
+    for (auto const& obj : init) {
+      std::string init_obj = "    (in " + obj.first + " " + obj.second + ")";
+      file << init_obj << std::endl;
+    }
+
+    // Is turtlebot always hand empty at start?
+    file << "    (hand_empty turtlebot)" << std::endl;
+    file << "  )\n" << std::endl;
+
+    // Goal section
+    file << "  (:goal" << std::endl;
+    // Hardcoded - should get it from topic
+    std::string goal = "(in apple living-room)";
+    file << "    " << goal << std::endl;
+    file << "  )" << std::endl;
+    file << ")" << std::endl;
+
+    file << ")" << std::endl;
+    file.close();
 }
 
 int main(int argc, char **argv)
